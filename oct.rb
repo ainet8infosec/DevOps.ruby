0c0be4092a3e7ec750303f65673080a0fd031cc4
Alexandros A. Iosifidis <al3k-t0r@users.noreply.github.com>
2015-06-12 13:12:21 +0300

    Update oct.rb

--- oct.rb
+++ oct.rb
@@ -2,8 +2,9 @@ def oct(num)
 	num**8
 end
 
+getInput=lambda { puts "Enter Integer:" }
+
 while true
-getInput= lambda { puts "Enter Integer:" }
 getInput.call
 num1=Integer(gets.chomp) rescue false
 num1 ? (num1==-1? (begin puts "Bye, bye now!!!!"; break end) : (puts oct(num1))) : (puts "Not integer, try again!")
