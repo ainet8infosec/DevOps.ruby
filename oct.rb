7b12bfac213ad33996b003633bee166208f14d7b
Alexandros A. Iosifidis <ainet8infosec@users.noreply.github.com>
2019-03-05 13:28:41 +0100

    Update oct.rb

--- oct.rb
+++ oct.rb
@@ -7,6 +7,6 @@ getInput=lambda { puts "Enter Integer:" }
 while true
 getInput.call
 num1=Integer(gets.chomp) rescue false
-num1 ? (num1==-1? (begin puts "Bye, bye now!!!!"; break end) : (puts oct(num1))) : (puts "Not integer, try again!")
+num1 ? (num1==-1? (begin puts "Bye, bye now!!!!"; break end) : (puts oct(num1))) : (puts "Not integer, try again!!!!")
 end
 	
